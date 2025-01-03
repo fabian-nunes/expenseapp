839b5504772bb48faa573a66a3d630f506ee078c
Fabian Nunes <fabiannunes12@gmail.com>
2025-01-03 20:01:05 +0000

    working on the chart screen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,13 +1,30 @@
 import 'dart:math';
 
 import 'package:expenseapp/screens/home/views/main_screen.dart';
+import 'package:expenseapp/screens/stats/stats.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var widgetList = [
+    MainScreen(),
+    StatScreen(),
+  ];
+
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
+    // Dynamic colors based on theme
+    Color selectedColor = Theme.of(context).colorScheme.primary;
+    Color unselectedColor = Theme.of(context).colorScheme.onSurface;
+
     return Scaffold(
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.only(
@@ -15,17 +32,31 @@ class HomeScreen extends StatelessWidget {
           topRight: Radius.circular(20),
         ),
         child: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
+          currentIndex: index,
+          selectedItemColor: selectedColor, // Improved selected item color handling
+          unselectedItemColor: unselectedColor, // Improved unselected item color
           backgroundColor: Colors.white,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
-          items: const [
+          items: [
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
+              icon: Icon(
+                Icons.home,
+                color: index == 0 ? selectedColor : unselectedColor,
+              ),
               label: 'Home',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.line_axis),
+              icon: Icon(
+                Icons.line_axis,
+                color: index == 1 ? selectedColor : unselectedColor,
+              ),
               label: 'Profile',
             ),
           ],
@@ -45,7 +76,7 @@ class HomeScreen extends StatelessWidget {
               colors: [
                 Theme.of(context).colorScheme.tertiary,
                 Theme.of(context).colorScheme.secondary,
-                Theme.of(context).colorScheme.primary
+                Theme.of(context).colorScheme.primary,
               ],
               transform: GradientRotation(pi / 4),
             ),
@@ -56,7 +87,7 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: const MainScreen(),
+      body: widgetList[index],
     );
   }
 }
---
+++ lib/screens/stats/stats.dart
@@ -0,0 +1,34 @@
+import 'package:expenseapp/screens/stats/chart.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class StatScreen extends StatelessWidget {
+  const StatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Transaction Stats',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              child: const MyChart(),
+              ),
+          ],
+            ),
+        ),
+      );
+  }
+}
